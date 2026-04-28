1755e935485ecae548079355479faabdf414af5e
quantmind-br <dsrodrigues20@gmail.com.br>
2026-04-28 10:29:14 -0300

    test(ui): exercise window size and assert quit symmetry

--- internal/ui/root_test.go
+++ internal/ui/root_test.go
@@ -11,6 +11,7 @@ import (
 
 func TestRoot_StartsOnProfilesAndQuitsOnQ(t *testing.T) {
 	tm := teatest.NewTestModel(t, NewRoot(TabProfiles), teatest.WithInitialTermSize(120, 30))
+	tm.Send(tea.WindowSizeMsg{Width: 120, Height: 30})
 
 	teatest.WaitFor(t, tm.Output(), func(out []byte) bool {
 		return strings.Contains(string(out), "Profiles")
@@ -34,5 +35,8 @@ func TestRoot_TabSwitchByNumber(t *testing.T) {
 	}, teatest.WithDuration(2*time.Second))
 
 	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'q'}})
-	_ = tm.Quit()
+
+	if err := tm.Quit(); err != nil {
+		t.Fatalf("Quit returned err: %v", err)
+	}
 }
